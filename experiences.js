cea9000a1556e4be2089406ea0ea5d8b29b1a207
ivn-svn <isvetllin@gmail.com>
2023-06-08 21:38:42 +0300

    add corrections to experiences.js

--- experiences.js
+++ experiences.js
@@ -1,28 +1,96 @@
-// Wait for the DOM to load before executing the JavaScript code
-document.addEventListener('DOMContentLoaded', function() {
-    var experienceTitle = document.querySelector('.experience-title');
-    var educationTitle = document.querySelector('.education-title');
-    var experiencePanel = document.getElementById('experience-panel');
-    var educationPanel = document.getElementById('education-panel');
-
-    // Show the IT EXPERIENCE panel by default
-    experienceTitle.classList.add('selected');
-    experiencePanel.style.display = 'block';
-
-    // Add event listeners to the title elements
-    experienceTitle.addEventListener('click', function() {
-        // Switch to IT EXPERIENCE
-        experienceTitle.classList.add('selected');
-        educationTitle.classList.remove('selected');
-        experiencePanel.style.display = 'block';
-        educationPanel.style.display = 'none';
-    });
-
-    educationTitle.addEventListener('click', function() {
-        // Switch to EDUCATION
-        experienceTitle.classList.remove('selected');
-        educationTitle.classList.add('selected');
-        experiencePanel.style.display = 'none';
-        educationPanel.style.display = 'block';
-    });
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const langSlider = document.querySelector('.langSlider');
+    const langItems = document.querySelectorAll('.langItem');
+    const enElements = document.querySelectorAll('.en');
+    const deElements = document.querySelectorAll('.de');
+    const bgElements = document.querySelectorAll('.bg');
+    const switchButtons = document.querySelectorAll('.switch-button');
+    const panels = document.querySelectorAll('.panel');
+  
+    class LanguageSelector {
+      constructor() {
+        this.init();
+      }
+  
+      init() {
+        langItems.forEach(item => {
+          item.addEventListener('click', () => {
+            const lang = item.dataset.lang;
+            this.showLanguage(lang);
+          });
+        });
+        this.showLanguage('en');
+      }
+  
+      showLanguage(lang) {
+        langItems.forEach(item => item.classList.remove('active'));
+        enElements.forEach(el => el.classList.add('hidden'));
+        deElements.forEach(el => el.classList.add('hidden'));
+        bgElements.forEach(el => el.classList.add('hidden'));
+  
+        const activeItem = document.querySelector(`.langItem[data-lang="${lang}"]`);
+        activeItem.classList.add('active');
+  
+        if (lang === 'en') {
+          enElements.forEach(el => el.classList.remove('hidden'));
+        } else if (lang === 'de') {
+          deElements.forEach(el => el.classList.remove('hidden'));
+        } else if (lang === 'bg') {
+          bgElements.forEach(el => el.classList.remove('hidden'));
+        }
+      }
+    }
+  
+    class SwitchPanel {
+      constructor() {
+        this.init();
+      }
+  
+      init() {
+        switchButtons.forEach(button => {
+          button.addEventListener('click', () => {
+            const target = button.dataset.target;
+            this.showPanel(target);
+          });
+        });
+        this.showPanel('experience-panel');
+      }
+  
+      showPanel(target) {
+        panels.forEach(panel => {
+          if (panel.id === target) {
+            panel.style.display = 'block';
+          } else {
+            panel.style.display = 'none';
+          }
+        });
+  
+        switchButtons.forEach(button => {
+          if (button.dataset.target === target) {
+            button.classList.add('selected');
+          } else {
+            button.classList.remove('selected');
+          }
+        });
+      }
+    }
+  
+    new LanguageSelector();
+    new SwitchPanel();
+  });
+  
+  function copyToClipboard(text) {
+    var tempInput = document.createElement("input");
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+  
+    var copyIcon = document.querySelector('.copy-icon');
+    copyIcon.classList.add('clicked');
+    setTimeout(function() {
+      copyIcon.classList.remove('clicked');
+    }, 1500);
+  }
+  
